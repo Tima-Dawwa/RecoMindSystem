944d3780dcab0fd68b8ead356d49032cb4544d58
Abdelaziz <abdeez.aushar@yandex.com>
2025-05-06 11:53:39 +0300

    Fixed template
    ready to edit

--- LogicBackend/routes/Dashboard/Products/products.validation.js
+++ LogicBackend/routes/Dashboard/Products/products.validation.js
@@ -1,6 +1,12 @@
 const Joi = require('joi');
 
+function validateCreateProduct(product) {
+    const schema = Joi.object({
+
+    });
+    return schema.validate(product, { abortEarly: false });
+}
 
 module.exports = {
-
+    validateCreateProduct
 }
