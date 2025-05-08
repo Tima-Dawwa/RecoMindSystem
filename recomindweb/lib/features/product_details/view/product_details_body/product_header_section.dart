38ef23582cf70667fc441a9f533bd8b83e62ed21
TimaDawaa <tima302t@gmail.com>
2025-05-08 19:27:34 +0300

    Product details page :
    header
    image section

---
+++ recomindweb/lib/features/product_details/view/product_details_body/product_header_section.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ProductHeader extends StatelessWidget {
+  const ProductHeader({super.key});
+
+//////fixes
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      alignment: Alignment.centerLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Relaxed Fit T-Shirt",
+            style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0.5,
+                ),
+          ),
+          const SizedBox(height: 12),
+          Text(
+            "Product Details",
+            style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey[700],
+                ),
+          ),
+          const SizedBox(height: 12),
+          const Text(
+            "Lorem ipsum flows with grace,"
+            "Color dances, finds its place,"
+            "In contrast, beauty shows its face.",
+            style: TextStyle(
+              fontSize: 16,
+              height: 1.6,
+              color: Colors.black87,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ recomindweb/lib/features/product_details/view/product_details_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:recomindweb/features/product_details/view/product_details_body/product_header_section.dart';
+import 'package:recomindweb/features/product_details/view/product_details_body/product_image_section.dart';
+
+class ProductPage extends StatefulWidget {
+  const ProductPage({super.key});
+
+  @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+//////fixes
+class _ProductPageState extends State<ProductPage> {
+  late String _selectedImage;
+  final List<String> _images = [
+    'assets/main_side.png',
+    'assets/front_side.png',
+    'assets/back_side.png',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedImage = _images[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ProductImageSection(
+                selectedImage: _selectedImage,
+                imageList: _images,
+                onThumbnailClick: (String newImage) {
+                  setState(() {
+                    _selectedImage = newImage;
+                  });
+                },
+              ),
+              const SizedBox(width: 16),
+              Expanded(child: ProductHeader()),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
