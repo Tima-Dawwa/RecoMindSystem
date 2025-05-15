1e8cb56f9e2ebc23a819b8944d63cd565181350b
Abdelaziz <abdeez.aushar@yandex.com>
2025-05-15 08:52:40 +0300

    Feature
    Added initial embedding file for content based

--- AIRecoMind/routers/recommendation.py
+++ AIRecoMind/routers/recommendation.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Query
-from services.content_based import get_recommendations_handler
+from fastapi import APIRouter, Response
+from services.content_based import get_content_based_recommendations, add_product
 from models.product import Product
 from typing import List
 
@@ -8,4 +8,10 @@ router = APIRouter()
 
 @router.get("/recommendations", response_model=List[Product])
 async def get_recommendations(product_id: str, top_n: int = 3):
-    return await get_recommendations_handler(product_id, top_n)
+    return await get_content_based_recommendations(product_id, top_n)
+
+
+@router.post("/recommendations")
+async def add_product_for_recommendation(product_id: str):
+    await add_product(product_id)
+    return Response(status_code=200, content=b"")
