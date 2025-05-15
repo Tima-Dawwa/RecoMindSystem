94401621bdec8ede6531ab86e98f86ec2ef18d9e
TimaDawaa <tima302t@gmail.com>
2025-05-15 18:38:15 +0300

    fixes

--- recomindweb/lib/features/product_details/view/product_details_body/product_image_section.dart
+++ recomindweb/lib/features/product_details/view/product_details_body/product_image_section.dart
@@ -15,51 +15,48 @@ class ProductImageSection extends StatelessWidget {
   Widget build(BuildContext context) {
     final bool showThumbnails = imageList.length > 1;
 
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          if (showThumbnails)
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: imageList.map((image) {
-                final isSelected = image == selectedImage;
-                return GestureDetector(
-                  onTap: () => onThumbnailClick(image),
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(vertical: 6),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: isSelected ? Colors.black : Colors.transparent,
-                        width: 3,
-                      ),
-                      borderRadius: BorderRadius.circular(8),
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        if (showThumbnails)
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: imageList.map((image) {
+              final isSelected = image == selectedImage;
+              return GestureDetector(
+                onTap: () => onThumbnailClick(image),
+                child: Container(
+                  margin: const EdgeInsets.symmetric(vertical: 6),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: isSelected ? Colors.black : Colors.transparent,
+                      width: 3,
                     ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(6),
-                      child: Image.asset(
-                        image,
-                        width: 110,
-                        height: 150,
-                        fit: BoxFit.cover,
-                      ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(6),
+                    child: Image.asset(
+                      image,
+                      width: 90,
+                      height: 120,
+                      fit: BoxFit.cover,
                     ),
                   ),
-                );
-              }).toList(),
-            ),
-          const SizedBox(width: 32),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.asset(
-              selectedImage,
-              height: 500,
-              fit: BoxFit.contain,
-            ),
+                ),
+              );
+            }).toList(),
           ),
-        ],
-      ),
+        const SizedBox(width: 32),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: Image.asset(
+            selectedImage,
+            height: 400,
+            fit: BoxFit.contain,
+          ),
+        ),
+      ],
     );
   }
 }
