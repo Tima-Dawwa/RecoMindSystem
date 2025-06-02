0b73a9ca04a8fb4b6f1db411a978619dc96b201f
sara najati <saranajatilaptop@gmail.com>
2025-06-02 09:45:43 +0300

    helpers classes

---
+++ recomindweb/lib/core/helpers/failure.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: avoid_print
+import 'package:dio/dio.dart';
+import 'package:recomindweb/core/helpers/status_code_handler.dart';
+
+class Failure {
+  final String? errTitle;
+  final dynamic errMessage;
+  final DioExceptionType? errType;
+  final StatusCodeHandler? handler;
+
+  Failure({
+    required this.errMessage,
+    this.errTitle,
+    this.errType,
+    this.handler,
+  });
+
+  factory Failure.fromDioException(
+    DioException dioException,
+    StatusCodeHandler handler,
+  ) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+      case DioExceptionType.sendTimeout:
+      case DioExceptionType.receiveTimeout:
+      case DioExceptionType.connectionError:
+      case DioExceptionType.unknown:
+        print('dio exc error is : ${dioException..error.toString()}');
+        return Failure(
+          errTitle: 'Error',
+          errMessage: "There seems to be a probleme with your Network Connection",
+          errType: dioException.type,
+        );
+      case DioExceptionType.badCertificate:
+      case DioExceptionType.cancel:
+        return Failure(
+          errTitle: 'Error',
+          errMessage: "Something went wrong\nRefresh or try again later",
+          errType: dioException.type,
+        );
+      case DioExceptionType.badResponse:
+        return handler.handleError(
+          dioException.response!.statusCode!,
+          dioException.response,
+        );
+    }
+  }
+}
