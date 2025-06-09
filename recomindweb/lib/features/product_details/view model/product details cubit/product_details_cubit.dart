09be45fbaff9dff3e84c0c7593c914a687509190
TimaDawaa <tima302t@gmail.com>
2025-06-09 12:07:22 +0300

    make cubit functionality

--- recomindweb/lib/features/product_details/view model/product details cubit/product_details_cubit.dart
+++ recomindweb/lib/features/product_details/view model/product details cubit/product_details_cubit.dart
@@ -1,6 +1,96 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:recomindweb/features/product_details/view%20model/product%20details%20cubit/product_details_state.dart';
+import 'package:recomindweb/core/helpers/failure.dart';
+import 'package:recomindweb/features/product_details/models/product_response.dart';
+import 'package:recomindweb/features/product_details/view model/product details cubit/product_details_state.dart';
+import 'package:recomindweb/features/product_details/view model/product_details_service.dart';
 
 class ProductDetailsCubit extends Cubit<ProductDetailsState> {
-  ProductDetailsCubit() : super(InitialProductDetails());
+  final ProductDetailsService productDetailsService;
+
+  ProductDetailsCubit(this.productDetailsService)
+    : super(InitialProductDetails());
+
+  Future<void> fetchProduct(String productId) async {
+    emit(LoadingProductDetails());
+    final result = await productDetailsService.getOneProduct(
+      productId: productId,
+    );
+
+    result.fold((failure) => emit(FailureProductDetails(failure: failure)), (
+      data,
+    ) {
+      try {
+        final productResponse = ProductResponse.fromJson(data);
+        emit(
+          SuccessProductDetails(
+            product: productResponse.data,
+            recommendations: productResponse.recommendations,
+          ),
+        );
+      } catch (e) {
+        emit(
+          FailureProductDetails(
+            failure: Failure(
+              errTitle: "Parsing Error",
+              errMessage: e.toString(),
+            ),
+          ),
+        );
+      }
+    });
+  }
+
+  Future<void> addReview({
+    required String productId,
+    required int rating,
+    required String commit,
+  }) async {
+    emit(LoadingProductDetails());
+    final result = await productDetailsService.addReview(
+      productId: productId,
+      rating: rating,
+      commit: commit,
+    );
+    result.fold(
+      (failure) => emit(FailureProductDetails(failure: failure)),
+      (_) => emit(ActionSuccessProductDetails()),
+    );
+  }
+
+  Future<void> addToFavorites(String productId) async {
+    emit(LoadingProductDetails());
+    final result = await productDetailsService.addToFavorites(
+      productId: productId,
+    );
+    result.fold(
+      (failure) => emit(FailureProductDetails(failure: failure)),
+      (_) => emit(ActionSuccessProductDetails()),
+    );
+  }
+
+  Future<void> deleteFavorite(String favoriteId) async {
+    emit(LoadingProductDetails());
+    final result = await productDetailsService.deleteFavorite(
+      favoriteId: favoriteId,
+    );
+    result.fold(
+      (failure) => emit(FailureProductDetails(failure: failure)),
+      (_) => emit(ActionSuccessProductDetails()),
+    );
+  }
+
+  Future<void> addToCart({
+    required String productId,
+    required int count,
+  }) async {
+    emit(LoadingProductDetails());
+    final result = await productDetailsService.addToCart(
+      productId: productId,
+      count: count,
+    );
+    result.fold(
+      (failure) => emit(FailureProductDetails(failure: failure)),
+      (_) => emit(ActionSuccessProductDetails()),
+    );
+  }
 }
