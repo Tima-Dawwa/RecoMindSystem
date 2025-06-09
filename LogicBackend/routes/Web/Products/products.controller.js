ff35cabdba20b5ed650e07f661e0d03d9f35a76c
Abdelaziz <abdeez.aushar@yandex.com>
2025-06-09 21:59:35 +0300

    Auto stash before merge of "Abdelaziz" and "origin/main"

--- LogicBackend/routes/Web/Products/products.controller.js
+++ LogicBackend/routes/Web/Products/products.controller.js
@@ -3,7 +3,7 @@ const { getPagination } = require('../../../services/query');
 const { validateProductRating } = require('./products.validation');
 const { normalizeBool, getCategory } = require('./products.helper');
 const { serializedData } = require('../../../services/serializeArray');
-const { productData, productDetailsData, interactionData } = require('./products.serializer');
+const { productData, productDetailsData, interactionData, productDataRecommendations } = require('./products.serializer');
 const { validationErrors } = require('../../../middlewares/validationErrors')
 const { getProducts, getProductById, getProductsCount, getProductsByIds, incrementInteractionCount, incrementRatingCount, applyChangedRatingToProduct } = require('../../../models/products.model');
 const { getProductInteractions, postInteraction, checkRating, updateRatingInteraction } = require('../../../models/interactions.model');
@@ -11,18 +11,18 @@ const { INTERACTION_TYPES } = require('../../../public/constants/interaction')
 const { checkFavorite } = require("../../../models/favorites.model");
 // Done
 async function httpGetAllProducts(req, res) {
-    const { skip, limit } = getPagination(req.query)
-    const { type, minPrice, maxPrice, isNew, isTrend } = req.query
-    const filters = {
-        type: type !== undefined ? getCategory(type) : [],
-        minPrice: minPrice !== undefined ? Number(minPrice) : 0,
-        maxPrice: maxPrice !== undefined ? Number(maxPrice) : 999999,
-        isNew: normalizeBool(isNew) ?? false,
-        isTrend: normalizeBool(isTrend) ?? false
-    };
-    const data = await getProducts(filters, skip, limit)
-    const length = await getProductsCount(filters)
-    return res.status(200).json({ data: serializedData(data, productData), count: length })
+  const { skip, limit } = getPagination(req.query)
+  const { type, minPrice, maxPrice, isNew, isTrend } = req.query
+  const filters = {
+    type: type !== undefined ? getCategory(type) : [],
+    minPrice: minPrice !== undefined ? Number(minPrice) : 0,
+    maxPrice: maxPrice !== undefined ? Number(maxPrice) : 999999,
+    isNew: normalizeBool(isNew) ?? false,
+    isTrend: normalizeBool(isTrend) ?? false
+  };
+  const data = await getProducts(filters, skip, limit)
+  const length = await getProductsCount(filters)
+  return res.status(200).json({ data: serializedData(data, productData), count: length })
 }
 
 // Done
@@ -48,14 +48,15 @@ async function httpGetOneProduct(req, res) {
       }
     );
     recommendedProducts = await getProductsByIds(recommendations.data);
-  } catch (error) {}
+  } catch (error) { }
   const interactions = await getProductInteractions(req.params.id);
   if (req.user) {
     product.isFavorite =
-      (await checkFavorite(req.user.id, req.params.id)) ?? false;
+      (await checkFavorite(req.user.id, req.params.id)) ? true : false;
     recommendedProducts = await Promise.all(
       recommendedProducts.map(async (p) => {
-        p.isFavorite = (await checkFavorite(req.user.id, p.id)) ?? false;
+        p.isFavorite = (await checkFavorite(req.user.id, p._id)) ? true : false;
+        p.parent_id = req.params.id
         return p;
       })
     );
@@ -65,39 +66,36 @@ async function httpGetOneProduct(req, res) {
   }
   return res.status(200).json({
     data: productDetailsData(product, interactions),
-    recommendations: serializedData(recommendedProducts, productData),
+    recommendations: serializedData(recommendedProducts, productDataRecommendations),
   });
 }
 
-
-
-
 // Done
 async function httpRateProduct(req, res) {
-    const { error } = validateProductRating(req.body)
-    if (error) {
-        return res.status(400).json({
-            errors: validationErrors(error.details)
-        })
-    }
-    const existingRating = await checkRating(req.user.id, req.params.id)
-    if (existingRating) {
-        const oldRatingValue = existingRating.rating_value;
-        await updateRatingInteraction(existingRating._id, req.body.rating)
-        await applyChangedRatingToProduct(req.params.id, oldRatingValue, req.body.rating);
-    }
-    else {
-        await postInteraction(req.user.id, req.params.id, INTERACTION_TYPES.RATING, req.body.rating, req.body.review_text)
-        await incrementRatingCount(req.params.id, req.body.rating)
-    }
+  const { error } = validateProductRating(req.body)
+  if (error) {
+    return res.status(400).json({
+      errors: validationErrors(error.details)
+    })
+  }
+  const existingRating = await checkRating(req.user.id, req.params.id)
+  if (existingRating) {
+    const oldRatingValue = existingRating.rating_value;
+    await updateRatingInteraction(existingRating._id, req.body.rating)
+    await applyChangedRatingToProduct(req.params.id, oldRatingValue, req.body.rating);
+  }
+  else {
+    await postInteraction(req.user.id, req.params.id, INTERACTION_TYPES.RATING, req.body.rating, req.body.review_text)
+    await incrementRatingCount(req.params.id, req.body.rating)
+  }
 
-    return res.status(200).json({
-        message: 'Product rated successfully',
-    });
+  return res.status(200).json({
+    message: 'Product rated successfully',
+  });
 }
 
 module.exports = {
-    httpGetAllProducts,
-    httpGetOneProduct,
-    httpRateProduct
+  httpGetAllProducts,
+  httpGetOneProduct,
+  httpRateProduct
 };
