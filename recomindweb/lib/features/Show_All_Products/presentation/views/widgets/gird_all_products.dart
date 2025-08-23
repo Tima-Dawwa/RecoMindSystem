0692bdbf790c577364ee0bcd6d4c698c0461b4e1
hamza Tinawi <hh5267852@gmail.com>
2025-08-23 12:43:19 +0300

    ..

--- recomindweb/lib/features/Show_All_Products/presentation/views/widgets/gird_all_products.dart
+++ recomindweb/lib/features/Show_All_Products/presentation/views/widgets/gird_all_products.dart
@@ -7,54 +7,6 @@
 
 //   final double price;
 
-<<<<<<< Updated upstream
-//   @override
-//   Widget build(BuildContext context) {
-//     // TODO: Replace with actual product data from your backend
-//     final List<Product> dummyProducts = List.generate(
-//       12,
-//       (index) => Product(
-//         name: 'Product ${index + 1}',
-//         price: (index + 1) * 100.0,
-//         rating: 4.0 + (index % 2 == 0 ? 0.5 : 0.0),
-//         imageUrl: 'assets/main_image.jpg',
-//         gender: 'male',
-//         category: 'ss',
-//       ),
-//     );
-
-//     return Expanded(
-//       child: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: GridView.builder(
-//           gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-//             maxCrossAxisExtent: 280,
-//             childAspectRatio: 0.75,
-//             crossAxisSpacing: 16,
-//             mainAxisSpacing: 16,
-//           ),
-//           itemCount: dummyProducts.length,
-//           itemBuilder: (context, index) {
-//             return ProductCard(
-//               product: dummyProducts[index],
-//               onTap: () {
-//                 // TODO: Implement product detail navigation
-//                 ScaffoldMessenger.of(context).showSnackBar(
-//                   SnackBar(
-//                     content: Text(
-//                       'Selected product: ${dummyProducts[index].name}',
-//                     ),
-//                   ),
-//                 );
-//               },
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
-=======
   @override
   Widget build(BuildContext context) {
     final List<Product> dummyProducts = List.generate(
@@ -99,4 +51,3 @@
     );
   }
 }
->>>>>>> Stashed changes
