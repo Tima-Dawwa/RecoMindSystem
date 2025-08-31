4e788dfe227b1a6089bb2f425194d9955d606451
Abdelaziz <abdeez.aushar@yandex.com>
2025-08-31 12:43:34 +0300

    malek fix

--- LogicBackend/routes/Web/Orders/orders.controller.js
+++ LogicBackend/routes/Web/Orders/orders.controller.js
@@ -1,5 +1,5 @@
 const { resetCart } = require('../../../models/cart.model');
-const { getOrdersForUser, getOrder, createOrder } = require('../../../models/orders.model');
+const { getOrdersForUser, getOrder, createOrder, getOrdersCountForUser } = require('../../../models/orders.model');
 const { getPagination } = require('../../../services/query');
 const { getProductsByIds, decrementQuantity } = require('../../../models/products.model');
 const { createPaymentData } = require('../../../services/payment');
