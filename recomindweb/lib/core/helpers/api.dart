daf3575672a1f37dfd999c6df745598f38ab9819
hamza Tinawi <hh5267852@gmail.com>
2025-09-05 18:40:23 +0300

    ..

--- recomindweb/lib/core/helpers/api.dart
+++ recomindweb/lib/core/helpers/api.dart
@@ -7,16 +7,6 @@ class Api {
   Api(this._dio);
   final Dio _dio;
   final String baseUrl = 'https://aababedef0c5.ngrok-free.app';
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-
-  String token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4YmFkZWJiZTkwOThmNTM2NWZhZGUwZCIsIm5hbWUiOnsiZmlyc3RfbmFtZSI6ImhhbXoiLCJsYXN0X25hbWUiOiJ0aSJ9LCJpYXQiOjE3NTcwODAyODgsImV4cCI6MTc1NzMzOTQ4OH0.BYsWN94rwjxx2bMBovvBL2cCDlecwuUtKOdlmX23s8c";
-
->>>>>>> Stashed changes
-=======
->>>>>>> final
 
   Future<Map<String, dynamic>> get({
     required String endPoint,
