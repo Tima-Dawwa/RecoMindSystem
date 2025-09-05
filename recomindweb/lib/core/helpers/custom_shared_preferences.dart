52fbdb339db885be00ba4006d3d780062e8719bd
sara najati <saranajatilaptop@gmail.com>
2025-09-05 16:38:26 +0300

    fix

--- recomindweb/lib/core/helpers/custom_shared_preferences.dart
+++ recomindweb/lib/core/helpers/custom_shared_preferences.dart
@@ -1,9 +1,11 @@
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:html' as html;
 
 class CustomSharedPreferences {
   static const _key = "token";
 
-Future<void> saveToken(String token) async {
+  Future<void> saveToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString(_key, token);
   }
@@ -16,15 +18,20 @@ Future<void> saveToken(String token) async {
   Future<void> clearToken() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove(_key);
+    if (kIsWeb) {
+      html.window.localStorage.clear();
+    }
   }
 
   Future<bool> logged() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.containsKey(_key);
+    return (prefs.containsKey(_key) ||
+        html.window.localStorage.toString() != '{}');
   }
 
   Future<bool> cleared() async {
     final prefs = await SharedPreferences.getInstance();
-    return !prefs.containsKey(_key);
+    return (!prefs.containsKey(_key) ||
+        html.window.localStorage.toString() == '{}');
   }
 }
